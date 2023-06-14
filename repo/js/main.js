ffbef5fcf035846cb3ae0214e530e34007cc7d8f
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-13 21:50:54 -0400

    g

--- repo/js/main.js
+++ repo/js/main.js
@@ -3,15 +3,16 @@ const axios = require('axios')
 async function getWeatherData(zipCode) {
     try {
         const apiKey = 'aea26e52813982d4b13611732c43f397'
-    const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}'
+    const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=aea26e52813982d4b13611732c43f397'
 
     const response = await axios.get(apiUrl)
 
     const weatherData = reponse.data 
 
-    const temp = document.getElementById('test')
+    const temp = document.getElementById('temp')
     const temperature = weatherData.main.temp
-    temperature.innerHTML = temperature
+
+    temp.innerHTML = 'g'
 
     } catch (error) {
 

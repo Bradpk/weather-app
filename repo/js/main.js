62ccb75e1d637efcf81b5e6f37d1ae32acc462d4
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-06-13 21:23:49 -0400

    g

--- repo/js/main.js
+++ repo/js/main.js
@@ -1,1 +1,20 @@
+const axios = require('axios')
+
+async function getWeatherData(zipCode) {
+    try {
+        const apiKey = 'aea26e52813982d4b13611732c43f397'
+    const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid={API key}'
+
+    const response = await axios.get(apiUrl)
+
+    const weatherData = reponse.data 
+
+    const temp = document.getElementById('test')
+    const temperature = weatherData.main.temp
+    temperature.innerHTML = temperature
+
+    } catch (error) {
+
+    }
+}
 

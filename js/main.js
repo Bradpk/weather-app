6ce74d3b1fcf7d36e45bedad12cd38e2c1bf5cdf
Brad Kerr <134312764+Bradpk@users.noreply.github.com>
2023-09-21 22:38:29 -0400

    g

--- js/main.js
+++ js/main.js
@@ -29,7 +29,7 @@ function verifyZipcode(zipcode) {
 // This uses axios to retrieve weather data from openweathermap.org 
 async function retrieveData(zipcode) {
     try {
-        let weatherData = await axios.get(`http://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${apiKey}`) //fetch maybe?
+        let weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?zip=${zipcode}&appid=${apiKey}`) //fetch maybe?
         console.log(weatherData.data)
         return weatherData.data
     } catch (error) {
